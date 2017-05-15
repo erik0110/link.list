eb51599421ea57f565a3bd01703b9ae934ce9171
Dan Alvarez <danalvarez5280@gmail.com>
2017-05-15 10:35:42 -0600

    Got rid of JavaScript, added Jquery, got rid of second half of html, some css changes.

---
+++ link.list/scripts.js
@@ -0,0 +1,9 @@
+$('.enter').on('click', function () {
+  var newText = $('#websites').val();
+  $('#web1').text(newText);
+});
+
+$('.enter').on('click', function () {
+  var newUrl = $('#url').val ();
+  $('#url1').text(newUrl);
+})
