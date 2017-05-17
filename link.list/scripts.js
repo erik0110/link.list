a9171ca00b2daeca5ae2135c0e35e24ac2c71c6a
Dan Alvarez <danalvarez5280@gmail.com>
2017-05-16 18:08:07 -0600

    Got jquery running for phase 1 and 2, messing with responsive

--- link.list/scripts.js
+++ link.list/scripts.js
@@ -1,9 +1,43 @@
-$('.enter').on('click', function () {
-  var newText = $('#websites').val();
-  $('#web1').text(newText);
-});
+var bookmarkCounter = 0;
 
-$('.enter').on('click', function () {
-  var newUrl = $('#url').val ();
-  $('#url1').text(newUrl);
-})
+$('.enter').on('click', addCards)
+
+
+$('#right-half').on('click', '#delete', removeCard)
+                .on('click', '#read', readEnabled );
+
+$('#inputs').on('keydown', validateEnter);
+function removeCard(e) {
+  $(e.target).parent().remove();
+}
+function readEnabled(e) {
+  $(e.target).toggleClass('read-enabled')
+}
+function addCards(e) {
+  e.preventDefault();
+  var newSite = $('#websites').val();
+  var newUrl = $('#urlInput').val();
+  bookmarkCounter++;
+  appenBookmark(bookmarkCounter, newSite, newUrl)
+  }
+function appenBookmark(bookmarkCounter, newSite, newUrl){
+  $('#right-half').append(`
+    <div id="book-${bookmarkCounter}" class="bookmark">
+      <p id="web-${bookmarkCounter}" class="titles">${newSite}</p>
+      <hr />
+      <p id="url-${bookmarkCounter}" class="urls underlined">${newUrl}</p>
+      <hr />
+      <button id="read" class="right-buttons underlined">Read</button>
+      <button id="delete" class="right-buttons underlined">Delete</button>
+    </div>
+    `)
+}
+function validateEnter() {
+  var newSite = $('#websites').val();
+  var newUrl = $('#urlInput').val();
+  if ((newSite === "") || (newUrl === "")) {
+    $('.enter').attr('disabled', true);
+  }
+  else { $('.enter').attr('disabled', false)
+  }
+}
